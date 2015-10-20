d951d25629b82d661171e75d58a7827613a043b5
N00147653 <N00147653@CL-C012-29.institute.local>
2015-10-20 11:45:45 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sample Form</title>
+        <style>
+            div.container {
+                margin: auto;
+                width: 960px;
+            }
+            div.row{
+                display:block;
+                margin-top:10px;
+            }
+            div.label{
+                padding-right:10px;
+            }
+            div.control{
+                display: inline-block;
+            }
+
+        </style>
+    </head>
+    <body>
+
+        <form action="process.php" method="post">
+            <div class="container">
+                <div class="row">
+                    <div class="label">
+                        <label>Username</label>
+                    </div>
+                    <div class="control">
+                        <input type="text" name="username"/>
+                    </div>
+                </div>
+
+            <div class="row">
+                   <div class="label">
+                        <label>Password</label>
+                    </div>
+                    <div class="control">
+                        <input type="password" id="password" name="password"/>
+                    </div>
+                </div>
+                <div class="error">
+                    <span id="passwordError"></span>
+                </div>
+
+                <div class="row">
+                    <div class="label">
+                        <label>Age</label>
+                    </div>
+                    <div class="control">
+                        <input type="text" name="age"/>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="label">
+                        <label>Date of birth</label>
+                    </div>
+                    <div class="control">
+                        <input type="date" name="dob"/>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="label">
+                        <label>Email</label>
+                    </div>
+                    <div class="control">
+                        <input type="email" name="email"/>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="label">
+                        <label>Website</label>
+                    </div>
+                    <div class="control">
+                        <input type="url" name="website"/>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="label">
+                        <label>Operating System</label>
+                    </div>
+                    <div class="control">
+
+                        <input type="radio" name="os" value="windows"/> Windows
+                        <input type="radio" name="os" value="macosx"/> Mac OS X
+                        <input type="radio" name="os" value="linux"/> Linux
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="label">
+                        <label>Programming Languages</label>
+                    </div>
+                    <div class="control">
+
+                        <input type="checkbox" name="languages[]" value="C++"/> C++
+                        <input type="checkbox" name="languages[]" value="Java"/> Java
+                        <input type="checkbox" name="languages[]" value="PHP"/> PHP
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="label"></div>
+                    <div class="control">
+                        <input type="submit" value="Submit"/>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </body>
+</html>
+
---
+++ process.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <pre>
+            <?php
+            print_r($_POST);
+
+            $input_method = INPUT_POST;
+            $formdata = array();
+            $errors = array();
+
+            $formdata['username'] = filter_input($input_method, "username", FILTER_SANITIZE_STRING);
+            $formdata['password'] = filter_input($input_method, "password", FILTER_SANITIZE_STRING);
+            $formdata['age'] = filter_input($input_method, "age", FILTER_SANITIZE_NUMBER_INT);
+            $formdata['dob'] = filter_input($input_method, "email", FILTER_SANITIZE_EMAIL);
+            $formdata['website'] = filter_input($input_method, "website", FILTER_SANITIZE_URL);
+            $formdata['os'] = filter_input($input_method, "os", FILTER_SANITIZE_STRING);
+            $formdata['languages'] = filter_input($input_method, "languages", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
+            $formdata['skills'] = filter_input($input_method, "skills", FILTER_SANITIZE_STRING);
+            $formdata['devices'] = filter_input($input_method, "devices", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
+            $formdata['comments'] = filter_input($input_method, "comments", FILTER_SANITIZE_STRING);
+            $formdata['picture'] = filter_input($input_method, "picture", FILTER_SANITIZE_STRING);
+
+
+            print_r($formdata);
+
+            if ($formdata['username'] === NULL ||
+                    $formdata['username'] === FALSE ||
+                    $formdata['username'] === "") {
+                $errors['username'] = "Username required";
+            }
+
+            if ($formdata['password'] === NULL ||
+                    $formdata['password'] === FALSE ||
+                    $formdata['password'] === "") {
+                $errors['password'] = "Password required";
+            }
+
+            if ($formdata['age'] !== "") {
+                $age = intval($formdata['age']);
+                if ($age < 0 || $age > 120) {
+                    $errors['age'] = "Age must be between 0 and 120 inclusive";
+                }
+            }
+
+
+            if ($formdata['dob'] !== NULL && $formdata['dob'] !== FALSE && $formdata['dob'] !== "") {
+                $test_arr = explode('/', $formdata['dob']);
+                if (count($test_arr) == 3) {
+                    if (checkdata($test_arr[0], $test_arr[1], $test_arr[2])) {
+                        //valid date    
+                    } else {
+                        //problem with dates
+                    }
+                } else {
+                    //problem with input
+                }
+            }
+            
+            if ($formdata['email'] !== NULL && $formdata['email'] !== FALSE && $formdata['email'] !== "") {
+                if (!filter_var($formdata['email'], FILTER_VALIDATE_EMAIL)) {
+                    $errors ['email'] = "Invalid email address format";
+                }
+            }
+
+            if ($formdata['website'] !== NULL && $formdata['website'] !== FALSE && $formdata['website'] !== "") {
+                if (!filter_var($formdata['website'], FILTER_VALIDATE_URL)) {
+                    $errors['website'] = "Invalid website URL format";
+                }
+            }
+
+            if ($formdata['os'] !== NULL && $formdata['os'] !== FALSE && $formdata['os'] !== "") {
+                $validOperSystems = array("windows", "macosx", "linux");
+
+                if (!in_array($formdata['os'], $validOperSystems)) {
+                    $errors['os'] = "Invalid operating system";
+                }
+            }
+
+
+            if ($formdata['languages'] !== NULL && $formdata['languages'] !== FALSE && $formdata['languages'] !== "") {
+                $validLanguages = array("C++", "Java", "PHP");
+                foreach ($formdata['languages'] as $language) {
+                    if (!in_array($formdata['languages'], $validLanguages)) {
+                        $errors['languages'] = "Invalid programming language";
+                        break;
+                    }
+                }
+            }
+
+            if ($formdata['skill'] !== NULL && $formdata['skill'] !== FALSE && $formdata['skill'] !== "") {
+                $validSkills = array("beginner", "intermediate", "advanced");
+                if (!in_array($formdata['skills'], $validSkills)) {
+                    $errors['skills'] = "Invalid skill";
+                }
+            }
+
+            if ($formdata['devices'] !== NULL && $formdata['devices'] !== FALSE && $formdata['devices'] !== "") {
+                $validDevices = array("desktop", "laptop", "tablet", "smartphone");
+                foreach ($formdata['devices'] as $device) {
+                    if (!in_array($formdata['devices'], $validDevices)) {
+                        $errors['devices'] = "Invalid device";
+                        break;
+
+                        //REMINDER NOTE: finish validation for skills and devices as per video on form validation
+                    }
+                }
+            }
+            print_r($errors);
+
+            if (empty($errors)) {
+                echo '<p>form data is valid</p>';
+            } else {
+                echo '<p>form data is invalid</p>';
+            }
+            ?>
+        </pre>
+    </body>
+</html>
